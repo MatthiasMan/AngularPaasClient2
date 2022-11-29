20c9122914938ac1724848573c9fd1d39697f7e3
Peter Vadle <108583@fhwn.ac.at>
2022-11-29 17:02:22 +0100

    aa

--- src/app/main-window/main-window.component.ts
+++ src/app/main-window/main-window.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { QueueClient, QueueServiceClient } from '@azure/storage-queue';
 import * as signalR from '@microsoft/signalr';
 import { HubConnection, IHttpConnectionOptions } from '@microsoft/signalr';
@@ -17,24 +17,24 @@ import { SignalRService } from '../signal-r.service';
 export class MainWindowComponent implements OnInit {
   private queueServiceClient: QueueServiceClient;
   public client: QueueClient
-  constructor(private Service : PictureDoneService) { }
+  constructor(private Service: PictureDoneService) { }
   public hubConnection: HubConnection;
-  account:string ="queuestorage2022";
-  sas:string="?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2022-12-30T21:25:30Z&st=2022-11-26T13:25:30Z&spr=https&sig=yTS3k8yPAkdmYva8UfhMapkwi%2BsKDuGAX%2FPhYeJakS0%3D";
-@Input() width:string;
-@Input() height:string;
+  account: string = "queuestorage2022";
+  sas: string = "?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2022-12-30T21:25:30Z&st=2022-11-26T13:25:30Z&spr=https&sig=yTS3k8yPAkdmYva8UfhMapkwi%2BsKDuGAX%2FPhYeJakS0%3D";
+  @Input() width: string;
+  @Input() height: string;
   CreatePicture(): void {
     console.log(this.height);
     console.log(this.width);
     this.Service.sendUpdate('Message from Sender Component to Receiver Component!');
-}
+  }
 
   ngOnInit(): void {
     const { setLogLevel } = require("@azure/logger");
 
     setLogLevel("info");
 
-    this.queueServiceClient = new QueueServiceClient( `https://${this.account}.queue.core.windows.net${this.sas}`);
+    this.queueServiceClient = new QueueServiceClient(`https://${this.account}.queue.core.windows.net${this.sas}`);
     this.client = this.queueServiceClient.getQueueClient("queueclin")
 
     const options: IHttpConnectionOptions = {
@@ -43,80 +43,76 @@ export class MainWindowComponent implements OnInit {
       }
     };
 
-    this.hubConnection = new signalR.HubConnectionBuilder()  
-      .configureLogging(signalR.LogLevel.Information)  
-      .withUrl("https://vamahubserver.azurewebsites.net/vamahub", options)  
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .configureLogging(signalR.LogLevel.Information)
+      .withUrl("https://vamahubserver.azurewebsites.net/vamahub", options)
       .withAutomaticReconnect()
-      .build();  
-      
-      this.hubConnection.start().then(function () {  
-        console.log('SignalR Connected!');  
-      }).catch(function (err) {  
-        return console.error(err.toString());  
-      });  
-    this.hubConnection.on("CalculatedMessage", (obj) =>
-    {
-        console.log("CalculationFinished");
-        this.Service.sendUpdate(obj as string);
-        //Saver.SaveImage(Saver.Convert((string)obj), "C:\\temp" + $"/Picture74.bmp");
-        //exit = true;
-    });
-  
+      .build();
 
-    this.hubConnection.on("ProgressMessage", (obj) =>
-    {
-        console.log("Current Progress: " + obj.ToString());
+    this.hubConnection.start().then(function () {
+      console.log('SignalR Connected!');
+    }).catch(function (err) {
+      return console.error(err.toString());
     });
-  
-    this.hubConnection.onclose(x =>
-    {
-        console.log("connection closed...");
+    this.hubConnection.on("CalculatedMessage", (obj) => {
+      console.log("CalculationFinished");
+      this.Service.sendUpdate(obj as string);
+      //Saver.SaveImage(Saver.Convert((string)obj), "C:\\temp" + $"/Picture74.bmp");
+      //exit = true;
     });
-    this.hubConnection.onreconnecting(x =>
-    {
-        console.log("reconnecting...");
+
+
+    this.hubConnection.on("ProgressMessage", (obj) => {
+      console.log("Current Progress: " + obj.ToString());
     });
-    this.hubConnection.onreconnected(x =>
-    {
-        console.log("reconnected...")
+
+    this.hubConnection.onclose(x => {
+      console.log("connection closed...");
+    });
+    this.hubConnection.onreconnecting(x => {
+      console.log("reconnecting...");
+    });
+    this.hubConnection.onreconnected(x => {
+      console.log("reconnected...")
     });
   }
 
   run() {
 
 
-    /*console.log("starting...")
+    console.log("starting...")
 
 
     var req = new CalculationRequest();
 
     req.Height = parseInt(this.height);
     req.Width = parseInt(this.width);
+    req.RequestId = this.newGuid();
     req.CalculationId = this.hubConnection.connectionId!;
     req.Parts = 4.0;
     req.XReminder = -2.0;
     req.YReminder = 1.0;
     req.Step = 0.03;
-req.MaxBetrag = 4,
-req.MaxIterations = 18;
-    var jsonReq = JSON.stringify(req);*/
+    req.MaxBetrag = 4,
+    req.MaxIterations = 18;
+    var jsonReq = JSON.stringify(req);
 
-    let msg = '{\"RequestId\":\"" + this.newGuid() + "\",\"Height\":" + this.height +",\"Width\":"+ this.width +",\"CalculationId\":\"" + this.hubConnection.connectionId + "\",\"Parts\":" + "4.0" + ",\"XReminder\":" + "-2.0" +",\"YReminder\":"+ "1.0" + ",\"Step\":0.03,\"MaxBetrag\":4,\"MaxIterations\":18}'
-    console.log(msg);
+    /*let msg = '{\"RequestId\":\"" + this.newGuid() + "\",\"Height\":" + this.height +",\"Width\":"+ this.width +",\"CalculationId\":\"" + this.hubConnection.connectionId + "\",\"Parts\":" + "4.0" + ",\"XReminder\":" + "-2.0" +",\"YReminder\":"+ "1.0" + ",\"Step\":0.03,\"MaxBetrag\":4,\"MaxIterations\":18}'
+    console.log(msg);*/
 
-    const encode =btoa(msg);
+    const encode = btoa(jsonReq);
     console.log("e: " + encode);
     this.client.sendMessage(encode);
   }
 
-    newGuid() {
-      return 'xxxxxxxx-xxxx-4xxx-9xxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c)=> {
-        var r = Math.random() * 16 | 0,
-          v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-      });
-      
-      
-    }
-     
+  newGuid() {
+    return 'xxxxxxxx-xxxx-4xxx-9xxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      var r = Math.random() * 16 | 0,
+        v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+
+
+  }
+
 }
